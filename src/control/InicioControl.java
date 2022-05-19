faeca4bd760f91fddb6c03c7ac79f169794a787e
Pedrojimvi <95476024+Pedrojimvi@users.noreply.github.com>
2022-05-19 12:45:51 +0200

    Add files via upload

--- src/control/InicioControl.java
+++ src/control/InicioControl.java
@@ -8,6 +8,8 @@ import javax.swing.JMenuItem;
 
 import com.dam.view.VInicio;
 import com.dam.view.VPrincipal;
+import com.dam.view.PPedido;
+import com.dam.view.PVenta;
 import com.dam.view.VCrear;
 
 public class InicioControl implements ActionListener {
@@ -15,11 +17,15 @@ public class InicioControl implements ActionListener {
 	private VInicio vInicio;
 	private VPrincipal vPrincipal;
 	private VCrear vCrear;
+	private PPedido pPedido;
+	private PVenta pVenta;
 	
-	public InicioControl(VInicio vInicio, VPrincipal vPrincipal, VCrear vCrear) {
+	public InicioControl(VInicio vInicio, VPrincipal vPrincipal, VCrear vCrear, PPedido pPedido, PVenta pVenta) {
 		this.vInicio = vInicio;
 		this.vPrincipal = vPrincipal;
 		this.vCrear = vCrear;
+		this.pPedido = pPedido;
+		this.pVenta = pVenta;
 	}
 
 	@Override
@@ -37,26 +43,83 @@ public class InicioControl implements ActionListener {
 			else if (ev.getActionCommand().equals(VCrear.BTN_VOLVER)) {
 				volverInicio();
 			}
+			else if (ev.getActionCommand().equals(PVenta.BTN_FILTRAR)) {
+				filtrarVenta();
+			}
+			else if (ev.getActionCommand().equals(PVenta.BTN_ANADIR)) {
+				anadir();
+			}
+			else if (ev.getActionCommand().equals(PVenta.BTN_LIMPIAR)) {
+				//pVenta.resetearValores();
+			}
+			else if (ev.getActionCommand().equals(PVenta.BTN_QUITAR)) {
+				quitar();
+			}
+			else if (ev.getActionCommand().equals(PVenta.BTN_VENTA)) {
+				vender();
+			}
+			else if (ev.getActionCommand().equals(PPedido.BTN_FILTRAR)) {
+				filtrarPedido();
+			}
+			else if (ev.getActionCommand().equals(PPedido.BTN_ANADIR)) {
+				anadir();
+			}
+			else if (ev.getActionCommand().equals(PPedido.BTN_LIMPIAR)) {
+				//pPedido.resetearValores();
+			}
+			else if (ev.getActionCommand().equals(PPedido.BTN_PEDIDO)) {
+				pedir();
+			}
+			else if (ev.getActionCommand().equals(PPedido.BTN_QUITAR)) {
+				quitar();
+			}
 		}
 		else if (ev.getSource() instanceof JMenuItem) {
 			if (ev.getActionCommand().equals(VPrincipal.MNTM_PEDIDO)) {
 				mostrarPedido();
 			}
-			if (ev.getActionCommand().equals(VPrincipal.MNTM_VENTA)) {
+			else if (ev.getActionCommand().equals(VPrincipal.MNTM_VENTA)) {
 				mostrarVenta();
 			}
-			if (ev.getActionCommand().equals(VPrincipal.MNTM_CERRAR)) {
+			else if (ev.getActionCommand().equals(VPrincipal.MNTM_CERRAR)) {
+				vPrincipal.dispose();
 				volverInicio();
 			}
 		}
 	}
 
-	private void mostrarVenta() {
+	private void pedir() {
 		
 	}
 
-	private void mostrarPedido() {
+	private void filtrarPedido() {
 		
+	}
+
+	private void vender() {
+		
+	}
+
+	private void quitar() {
+		
+	}
+
+	private void anadir() {
+		
+	}
+
+	private void filtrarVenta() {
+		
+	}
+
+	private void mostrarVenta() {
+		//pVenta.resetearValores();
+		vPrincipal.cargarPanel(pVenta);
+	}
+
+	private void mostrarPedido() {
+		//pPedido.resetearValores();
+		vPrincipal.cargarPanel(pPedido);
 	}
 
 	private void crearUsuario() {
