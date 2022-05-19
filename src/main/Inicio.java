c9f279b31f86df850cd9082eac2ee93b1d3f5c10
Pedrojimvi <95476024+Pedrojimvi@users.noreply.github.com>
2022-05-19 12:48:58 +0200

    Add files via upload

--- src/main/Inicio.java
+++ src/main/Inicio.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import com.dam.control.InicioControl;
 import com.dam.view.VInicio;
 import com.dam.view.VPrincipal;
+import com.dam.view.PPedido;
+import com.dam.view.PVenta;
 import com.dam.view.VCrear;
 
 public class Inicio {
@@ -16,12 +18,16 @@ public class Inicio {
 				VInicio vInicio= new VInicio();
 				VPrincipal vPpal = new VPrincipal();
 				VCrear vCrear = new VCrear();
+				PPedido pPedido = new PPedido();
+				PVenta pVenta = new PVenta();
 				
-				InicioControl control = new InicioControl(vInicio, vPpal, vCrear);
+				InicioControl control = new InicioControl(vInicio, vPpal, vCrear, pPedido, pVenta);
 				
 				vInicio.setControlador(control);
 				vCrear.setControlador(control);
 				vPpal.setControlador(control);
+				pPedido.setControladorBotones(control);
+				pVenta.setControladorBotones(control);
 				
 				vInicio.hacerVisible();
 			}
