cdf03e0d8a703fe32878bca7f18c464a81827cef
Jesuusgs <98019367+Jesuusgs@users.noreply.github.com>
2022-06-04 12:46:34 +0200

    Update InicioControl.java

--- src/control/InicioControl.java
+++ src/control/InicioControl.java
@@ -2,12 +2,15 @@ package com.dam.control;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
 
 import com.dam.view.VInicio;
 import com.dam.view.VPrincipal;
+import com.dam.db.persistencia.FarmaciaPersistencia;
+import com.dam.model.Usuario;
 import com.dam.view.PPedido;
 import com.dam.view.PVenta;
 import com.dam.view.VCrear;
@@ -19,26 +22,76 @@ public class InicioControl implements ActionListener {
 	private VCrear vCrear;
 	private PPedido pPedido;
 	private PVenta pVenta;
+	private FarmaciaPersistencia fPersistencia;
 	
-	public InicioControl(VInicio vInicio, VPrincipal vPrincipal, VCrear vCrear, PPedido pPedido, PVenta pVenta) {
+	public InicioControl(VInicio vInicio, VPrincipal vPrincipal, VCrear vCrear, PPedido pPedido, PVenta pVenta, FarmaciaPersistencia fPersistencia) {
 		this.vInicio = vInicio;
 		this.vPrincipal = vPrincipal;
 		this.vCrear = vCrear;
 		this.pPedido = pPedido;
 		this.pVenta = pVenta;
+		this.fPersistencia = fPersistencia;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent ev) {
+		ArrayList<String> lista = new ArrayList<String>();
+		Usuario user = null;
+		
 		if (ev.getSource() instanceof JButton) {
 			if (ev.getActionCommand().equals(VInicio.BTN_INICIAR)) {
-				abrirPrincipal();
+				String nombreUsuario = vInicio.getNomUser();
+				String contrasena = vInicio.getContrasena();
+				
+				if (nombreUsuario.trim().isEmpty() || contrasena.trim().isEmpty()) {
+					vInicio.mostrarError("Debes rellenar los campos 'Usuario' y  'Password'");
+					
+				}else {
+					user = fPersistencia.validarUsuario(nombreUsuario);
+					
+					if (user != null) {
+						if (contrasena.equals(user.getPassword())) {
+							abrirPrincipal();
+						}else {
+							vInicio.mostrarError("La contraseña introducida no es correcta");
+						}
+					}else {
+						vInicio.mostrarError("El usuario introducido no está registrado");
+					}
+				}
 			}
 			else if (ev.getActionCommand().equals(VInicio.BTN_CREAR)) {
 				abrirCrear();
 			}
 			else if (ev.getActionCommand().equals(VCrear.BTN_CREAR)) {
-				crearUsuario();
+				String usuario = vCrear.getUser();
+				String pwd = vCrear.getContrasena();
+				String confirPwd = vCrear.getCContrasena();
+				if (usuario.trim().isEmpty() || pwd.trim().isEmpty() || confirPwd.trim().isEmpty()) {
+					vCrear.mostrarError("Debes rellenar todos los campos");
+				}else {
+					user = fPersistencia.validarUsuario(usuario);
+					if (user == null) {
+						boolean b = vCrear.comprobarContrasenas();
+						
+						if (b == true) {
+							int num = fPersistencia.crearUsuarioNuevo(usuario, pwd);
+							if (num == 1) {
+								vCrear.mostrarMensaje("El nuevo usuario ha sido registrado correctamente");
+								crearUsuario();
+							}
+							
+						}else {
+							vCrear.mostrarError("La contraseña debe coincidir con el campo 'Confirmar password'");
+						}
+					}else {
+						vCrear.mostrarError("El usuario que está intentando introducir ya está registrado. Por favor, introduzca otro");
+					}
+						
+					
+				}
+				
+				
 			}
 			else if (ev.getActionCommand().equals(VCrear.BTN_VOLVER)) {
 				volverInicio();
