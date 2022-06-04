a8a53ab258a7c639ac5a408fd3af4190cc9bce92
Jesuusgs <98019367+Jesuusgs@users.noreply.github.com>
2022-06-04 12:47:38 +0200

    Update Usuario.java

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,22 +1,16 @@
 package com.dam.model;
 
 public class Usuario {
-	private int id;
 	private String password;
 	private String apeNom;
 	
-	public Usuario(int id, String password, String apeNom) {
-		this.id = id;
+	public Usuario(String password, String apeNom) {
 		this.password = password;
 		this.apeNom = apeNom;
 	}
 
 	public String getApeNom() {
 		return apeNom;
-	}
-
-	public int getId() {
-		return id;
 	}
 
 	public String getPassword() {
