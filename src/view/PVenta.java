d1c8efd82697192ac0cb719907257f5fcd88e7da
Javier Martínez Gómez <79506253+javimartinez01@users.noreply.github.com>
2022-06-11 13:32:54 +0200

    Update PVenta.java

--- src/view/PVenta.java
+++ src/view/PVenta.java
@@ -1,17 +1,25 @@
 package com.dam.view;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import java.awt.Font;
+import java.util.ArrayList;
 
 import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JSpinner;
 import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
 
 import com.dam.control.InicioControl;
+import com.dam.db.persistencia.FarmaciaPersistencia;
+import com.dam.db.persistencia.MedicamentoContract;
+import com.dam.model.Medicamento;
 
 import java.awt.Color;
 import javax.swing.JTable;
@@ -26,6 +34,10 @@ public class PVenta extends JPanel {
 	public static final String BTN_LIMPIAR = "Limpiar";
 	public static final String BTN_QUITAR = "Quitar Producto";
 	public static final String BTN_VENTA = "Realizar Venta";
+	private static final String COL_NOMBRE = "NOMBRE";
+	private static final String COL_TIPO = "TIPO";
+	private static final String COL_CANTIDAD = "CANTIDAD";
+	private static final String COL_PRECIO = "PRECIO";
 	private JTable tblProd;
 	private JTable tblVenta;
 	private JButton btnQuitar;
@@ -36,6 +48,11 @@ public class PVenta extends JPanel {
 	private JButton btnAnadir;
 	private JButton btnLimpiar;
 	private JTextField txtNom;
+	private DefaultTableModel dtmMed;
+	private DefaultTableModel dtmMed2;
+	private JScrollPane scrpTblPr;
+	private ArrayList<Medicamento> listMed;
+	private JScrollPane scrpTblPe;
 	
 	public PVenta() {
 		setLayout(null);
@@ -109,6 +126,39 @@ public class PVenta extends JPanel {
 		txtNom.setColumns(10);
 	}
 	
+	private void configurarTabla() {
+		dtmMed = new DefaultTableModel() {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		
+		tblProd.setModel(dtmMed);
+		
+		dtmMed.addColumn(COL_NOMBRE);
+		dtmMed.addColumn(COL_TIPO);
+		dtmMed.addColumn(COL_CANTIDAD);
+		
+	}
+	
+	private void configurarTabla2() {
+		dtmMed2 = new DefaultTableModel() {
+			
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		
+		tblVenta.setModel(dtmMed2);
+		
+		dtmMed2.addColumn(COL_NOMBRE);
+		dtmMed2.addColumn(COL_TIPO);
+		dtmMed2.addColumn(COL_CANTIDAD);
+		dtmMed2.addColumn(COL_PRECIO);
+		
+	}
 	public void setControladorBotones(InicioControl  c) {
 		btnAnadir.addActionListener(c);
 		btnFiltrar.addActionListener(c);
@@ -122,5 +172,101 @@ public class PVenta extends JPanel {
 		txtNom.setText(null);
 		cmbTipo.setSelectedIndex(0);
 		spnCantidad.setValue(0);
+	}
+	
+	public void filtrarVenta(FarmaciaPersistencia fP) {
+		dtmMed.getDataVector().clear();
+    	tblVenta.setSelectionMode(0);
+    	
+    	String nom = txtNom.getText().trim();
+        String tipo = (String) cmbTipo.getSelectedItem();
+        
+        boolean esValido = true;
+        
+        String where = "";
+        
+        if (!nom.isBlank()) {
+			where += MedicamentoContract.NOMBRE + " LIKE ? ";
+		}
+        
+        if (!tipo.equals("Todos")) {			
+			if (!where.isBlank()) {
+				where += " AND ";
+			}
+			
+			where += MedicamentoContract.TIPO + " = ? ";
+		}
+        
+        if (esValido) {
+        	listMed = fP.seleccionarMed(nom, tipo, where, where);
+            
+            if (!listMed.isEmpty()) {
+            	for (Medicamento med : listMed) {
+    				dtmMed.addRow(med.getRowData());
+    			}
+			}
+            else {
+				mostrarMensaje("No hay ningún medicamento con esos filtros", "Error de búsqueda", 1);
+			}
+		}
+        
+	}
+	
+	private void mostrarMensaje(String mensaje, String titulo, int tipo) {
+		JOptionPane.showMessageDialog(this,mensaje, titulo, tipo);
+	}
+	
+	public void asignarTipos(InicioControl c) {
+        FarmaciaPersistencia rP = c.getfPersistencia();
+        
+        DefaultComboBoxModel<String> modelo = (DefaultComboBoxModel<String>) cmbTipo.getModel();
+            for (String region : rP.getTipos()) {
+                modelo.addElement(region);
+            }
+    }
+	
+	public void anadir() {		
+		Medicamento med;
+		try {
+			med = (Medicamento) listMed.toArray()[tblProd.getSelectedRow()];
+			
+			dtmMed2.addRow(med.getRowData2((int) spnCantidad.getValue()));
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			mostrarMensaje("No hay ningún medicamento seleccionado", "Error al Añadir", 0);
+		}
+		catch (NullPointerException e) {
+			mostrarMensaje("No hay ningún medicamento seleccionado", "Error al Añadir", 0);
+		}
+	}
+	
+	public void quitar() {
+		try {
+			dtmMed2.removeRow(tblVenta.getSelectedRow());
+		}
+			catch (ArrayIndexOutOfBoundsException e) {
+			mostrarMensaje("No hay ningún medicamento seleccionado", "Error al Quitar", 0);
+		}
+	}
+	
+	public void pedir(FarmaciaPersistencia fP) {
+		String nombre = null;
+		int cantidad = 0;
+		
+		if (dtmMed2.getRowCount() != 0) {
+			for (int i = 0; i < dtmMed2.getRowCount(); i++) {
+				nombre = (String) tblVenta.getModel().getValueAt(i, 0);
+				cantidad = (int) tblVenta.getModel().getValueAt(i, 5);
+				
+				fP.hacerPedido(cantidad, nombre);
+				
+				fP.modificarTblPedido(fP.obtenerIdMed(nombre, cantidad));
+			}
+			dtmMed2.getDataVector().clear();
+			mostrarMensaje("¡¡¡ Pedido realizado con éxito !!!", "Resultado de operación", 1);
+		}
+		else {
+			mostrarMensaje("No se puede hacer el pedido sin medicamentos", "Error al Hacer Pedido", 0);
+		}
 	}
 }
