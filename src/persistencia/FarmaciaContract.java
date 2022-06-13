4a184667d3606adce705b2e0e57a82a265c8d23e
Pedrojimvi <95476024+Pedrojimvi@users.noreply.github.com>
2022-06-13 09:54:03 +0200

    Update FarmaciaContract.java

--- src/persistencia/FarmaciaContract.java
+++ src/persistencia/FarmaciaContract.java
@@ -3,7 +3,7 @@ package persistencia;
 public class FarmaciaContract {
 	//TABLA EMPLEADO
 	public static final String NOM_TAB_EMP = "EMPLEADO";
-	public static final String EMP_CONTRASENA = "CONTRASEÑA";
+	public static final String EMP_CONTRASENA = "CONTRASENA";
 	public static final String EMP_APENOM = "APELLIDO_NOMBRE";
 	public static final String EMP_ID = "ID";
 }
